0905aa2329ff3d8892380d6f20fa10c152d5cf95
vincentwti <vincent.vincent@primecash.co.id>
2024-02-19 14:02:26 +0700

    add cat

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -22,3 +22,32 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 //     cube.rotation.y += 0.01;
 // }
 // animate();
+
+const planeWidth = 1;
+const planeHeight = 1;
+const geometry = new THREE.PlaneGeometry(planeWidth, planeHeight);
+ 
+const loader = new THREE.TextureLoader();
+ 
+makeInstance(geometry, 'white', 0, 'images/CatOutline.png');
+makeInstance(geometry, 'white',  0, 'images/Cat.png');
+
+function render() {
+    requestAnimationFrame(render);
+}
+
+function makeInstance(geometry, color, rotY, url) {
+    const texture = loader.load(url, render);
+    const material = new THREE.MeshPhongMaterial({
+      color,
+      map: texture,
+      opacity: 0.5,
+      transparent: true,
+      side: THREE.DoubleSide,
+    });
+   
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+   
+    mesh.rotation.y = rotY;
+}
