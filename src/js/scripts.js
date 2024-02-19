b7388af0672f609f1cfa85c6ae603da36da1036d
vincentwti <vincent.vincent@primecash.co.id>
2024-02-19 14:04:58 +0700

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
+//import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
