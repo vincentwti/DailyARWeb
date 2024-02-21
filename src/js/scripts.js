2690f53415dcaf58af45cc4b272ee9722a41ff3d
vincentwti <vincent.vincent@primecash.co.id>
2024-02-21 11:06:24 +0700

    add score & device camera

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,15 +5,17 @@ const gui = new dat.GUI()
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const loader = new THREE.TextureLoader();
-const targetInitialPos = new THREE.Vector3(-2, 0, 0);
+const targetInitialPos = new THREE.Vector3(-2, -0.7, 0);
 const targetSpeed = 0.2;
-const fps = 30;
+const fps = 60;
 
 let isPaused = false;
 
 let tryCount = 0;
 let width = window.innerWidth;
 let height = window.innerHeight;
+let targetDistance;
+let score = 0;
 
 scene.background = new THREE.Color(0x262626);
 camera.position.set(0, 0, 2);
@@ -37,7 +39,7 @@ const canvas = document.createElement('canvas');
 let context = canvas.getContext('2d');
 
 
-const outlineObj = makeInstance(geometry, 0, new THREE.Vector3(0, 0, 0), 'images/CatOutline.png');
+const outlineObj = makeInstance(geometry, 0, new THREE.Vector3(0, targetInitialPos.y, 0), 'images/CatOutline.png');
 const targetObj = makeInstance(geometry,  0, targetInitialPos, 'images/Cat.png');
 
 window.addEventListener('pointerdown', click);
@@ -52,8 +54,8 @@ window.addEventListener('resize', () => {
 
 canvas.style.position = 'absolute';
 canvas.style.zIndex = 1;
-canvas.width = 300;
-canvas.height = 100;
+canvas.width = 512;
+canvas.height = 256;
 container.append(renderer.domElement)
 renderer.render(scene, camera)
 
@@ -63,11 +65,9 @@ let scoreMaterial = null;
 
 //document.body.appendChild(canvas);
 
-
-//document.body.appendChild(canvas);
-
 displayInterface();
 createScoreMesh();
+deviceCamera();
 render();
 
 function createScoreMesh() {
@@ -80,9 +80,10 @@ function createScoreMesh() {
     transparent: true,
     side: THREE.DoubleSide,
   });
-  const scoreGeometry = new THREE.PlaneGeometry(1, 0.25, 1 );
+
+  const scoreGeometry = new THREE.PlaneGeometry(1, 0.5, 1 );
   scoreMesh = new THREE.Mesh(scoreGeometry, scoreMaterial);
-  scoreMesh.position.set(0, -1, 0);
+  scoreMesh.position.set(0, 1, 0);
   scene.add(scoreMesh);
 }
 
@@ -109,6 +110,12 @@ function click(event){
   {
     tryCount += 1;
     displayInterface();
+    targetDistance = Math.abs(targetObj.position.x - outlineObj.position.x);
+    score = Math.round(100 - targetDistance * 50);
+    if(score === 100)
+    {
+      score -= 0.41;
+    }
   }
   isPaused = !isPaused
 }
@@ -135,13 +142,27 @@ function makeInstance(geometry, rotY, position, url) {
 }
 
 function displayInterface() {
+  let fontSize = 42 + (score > 90 ? (score / 5) : 0);
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.font = '42px sans-serif';
+  context.font = fontSize + 'px sans-serif';
   context.fillStyle = 'transparent';
 
   context.fillRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = 'white'
   context.textAlign = 'center';
   context.textBaseline = 'middle';
-  context.fillText('Try Count : ' + tryCount, canvas.width / 2, canvas.height / 2);
+  context.fillText('Score : ' + score, canvas.width / 2, canvas.height / 2);
+}
+
+function deviceCamera() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+      const video = document.getElementById('webcam-video');
+      video.srcObject = stream;
+  })
+  .catch(error => {
+      console.error('Error accessing webcam:', error);
+      // Handle error appropriately, e.g., display a message to the user
+  });
+
 }
